03377c71fa0f6e2787096b4cf5f7727e2185c3c5
Andrei Skrinnik <rakyt4gin@gmail.com>
2022-05-04 11:21:35 +0300

    feat: implement routing

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useCustomSelector } from './customHooks/customHooks';
 
 const App: React.FC = () => {
   const select = useCustomSelector((state) => state.testReducer);
 
-  return <div>{select.id}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>{select.id}</div>} />
+        <Route path="/main/*" element={<div>MAin</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
