65fac8aef5cf1c261941091f69eaf5fd25cd85dd
Andrei Skrinnik <rakyt4gin@gmail.com>
2022-05-06 14:08:32 +0300

    feat: implement LonIn page

---
+++ project-management-app/src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../Form/Form';
+import styles from './LoginPage.module.scss';
+
+const LoginPage: React.FC = () => {
+  return (
+    <Form login password>
+      Login
+    </Form>
+  );
+};
+
+export default LoginPage;
