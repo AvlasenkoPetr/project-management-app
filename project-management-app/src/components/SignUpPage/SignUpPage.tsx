b0f967ea5774e6d0130af0a1aafe1e035b546b52
Andrei Skrinnik <rakyt4gin@gmail.com>
2022-05-06 14:06:33 +0300

    feat: implement SignUp page

---
+++ project-management-app/src/components/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../Form/Form';
+import styles from './SignUp.module.scss';
+
+const SignUp: React.FC = () => {
+  return (
+    <Form login password name passwordRepeat>
+      Sign Up
+    </Form>
+  );
+};
+
+export default SignUp;
