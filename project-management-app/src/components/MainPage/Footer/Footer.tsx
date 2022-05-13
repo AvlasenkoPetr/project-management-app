be2a2941372d801687fb6c1cad25de738a6d9239
AvlasenkoPetr <phone.petr@gmail.com>
2022-05-13 04:33:09 +0300

    feat: add media to footer

--- project-management-app/src/components/MainPage/Footer/Footer.tsx
+++ project-management-app/src/components/MainPage/Footer/Footer.tsx
@@ -7,13 +7,13 @@ const Footer: React.FC = () => {
     <footer className={styles.footer}>
       <Logo />
       <div className={styles.footer__links}>
-        <a href="https://github.com/AvlasenkoPetr" target="_blank">
+        <a href="https://github.com/AvlasenkoPetr" target="_blank" rel="noreferrer">
           AvlasenkoPetr
         </a>
-        <a href="https://github.com/rakyt4gin" target="_blank">
+        <a href="https://github.com/rakyt4gin" target="_blank" rel="noreferrer">
           rakyt4gin
         </a>
-        <a href="https://github.com/R5G1" target="_blank">
+        <a href="https://github.com/R5G1" target="_blank" rel="noreferrer">
           R5G1
         </a>
       </div>
