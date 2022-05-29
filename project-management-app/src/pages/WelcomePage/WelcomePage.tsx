43ab18d49f32442dc2b6679d50b8c4dd6801e69d
AvlasenkoPetr <phone.petr@gmail.com>
2022-05-29 13:31:39 +0300

    feat: add welcome page translate

--- project-management-app/src/pages/WelcomePage/WelcomePage.tsx
+++ project-management-app/src/pages/WelcomePage/WelcomePage.tsx
@@ -14,23 +14,20 @@ const WelcomePage: React.FC = () => {
         <div className={styles.content_section}>
           <div className={styles.content_section__title}>{t('welcomePage.titles.course')}</div>
           <div className={styles.content_section__description}>
-            RS School is free-of-charge and community-based education program conducted by The
-            Rolling Scopes developer community since 2013. Everyone can study at RS School,
-            regardless of age, professional employment, or place of residence. The mentors and
-            trainers of our school are front-end and javascript developers from different companies
-            and countries.
+            {t('welcomePage.description.course')}
           </div>
         </div>
         <div className={styles.content_section}>
           <div className={styles.content_section__title}>{t('welcomePage.titles.project')}</div>
           <div className={styles.content_section__description}>
-            Система управления проектами – приложение помогающее достичь поставленные задачи
-            отдельному человеку в команде или группе разработчиков.
+            {t('welcomePage.description.project')}
           </div>
         </div>
         <div className={styles.content_section}>
           <div className={styles.content_section__title}>{t('welcomePage.titles.team')}</div>
-          <div className={styles.content_section__description}>Все сделал Андрей.</div>
+          <div className={styles.content_section__description}>
+            {t('welcomePage.description.team')}
+          </div>
         </div>
       </div>
       <Footer></Footer>
